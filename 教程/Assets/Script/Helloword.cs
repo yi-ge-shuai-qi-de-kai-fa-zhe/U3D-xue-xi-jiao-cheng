e2046d5115c16182f43c72e249496fb8760b7f99
yi-ge-shuai-qi-de-kai-fa-zhe <2270638422@qq.com>
2019-02-15 08:30:48 +0800

    第一次学习u3d的提交

---
+++ 教程/Assets/Script/Helloword.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helloword : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("hello word!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
